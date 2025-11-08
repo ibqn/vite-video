ad5677cdbff6cee02cea76e9c99ddc85cd4fca62
Evgeny Bobkin <evgeny.bobkin@europace.de>
2025-11-08 11:07:38 +0100

    refactor: simplify plugin configuration in vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -5,14 +5,7 @@ import path from 'path'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [
-    react({
-      babel: {
-        plugins: [['babel-plugin-react-compiler']],
-      },
-    }),
-    tailwindcss(),
-  ],
+  plugins: [react({ babel: { plugins: [['babel-plugin-react-compiler']] } }), tailwindcss()],
   resolve: {
     alias: [{ find: '@', replacement: path.resolve(__dirname, 'src') }],
   },
