4d4349abf13564d429296614d85fe75481a32772
Evgeny Bobkin <evgeny.bobkin@europace.de>
2025-11-08 12:06:30 +0100

    feat: implement VideoPlayer in App component with sample video source

--- src/app.tsx
+++ src/app.tsx
@@ -1,3 +1,24 @@
+import { VideoPlayer } from './components/video'
+
+const videoPlayerOptions = {
+  responsive: true,
+  autoplay: true,
+  sources: [
+    {
+      // src: '//vjs.zencdn.net/v/oceans.mp4',
+      src: 'https://storage.googleapis.com/gtv-videos-bucket/sample/BigBuckBunny.mp4',
+      type: 'video/mp4',
+    },
+  ],
+}
+
 export const App = () => {
-  return <div className="text-3xl font-bold underline">hello</div>
+  return (
+    <div className="flex min-h-svh flex-col items-center justify-center gap-8 p-4">
+      <div className="text-3xl font-bold italic">video player</div>
+      <div className="w-full max-w-3xl">
+        <VideoPlayer options={videoPlayerOptions} />
+      </div>
+    </div>
+  )
 }
