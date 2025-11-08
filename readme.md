1acbd2b59cd57a7d5f7f7bf4fd70d1a8c128e4dc
Evgeny Bobkin <evgeny.bobkin@europace.de>
2025-11-08 10:57:39 +0100

    init project

---
+++ readme.md
@@ -0,0 +1,1 @@
+## Vite based template for react and tailwind
---
+++ vite.config.ts
@@ -0,0 +1,19 @@
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react'
+import tailwindcss from '@tailwindcss/vite'
+import path from 'path'
+
+// https://vitejs.dev/config/
+export default defineConfig({
+  plugins: [
+    react({
+      babel: {
+        plugins: [['babel-plugin-react-compiler']],
+      },
+    }),
+    tailwindcss(),
+  ],
+  resolve: {
+    alias: [{ find: '@', replacement: path.resolve(__dirname, 'src') }],
+  },
+})
